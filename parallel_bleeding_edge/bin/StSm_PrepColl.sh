03f04a32d0acc6eb2dc23e3e8b561cc352a44699
Pau Amaro Seoane <amaro@riseup.net>
2024-08-24 15:37:37 +0200

    Modified StSm_PrepColl.sh

--- parallel_bleeding_edge/bin/StSm_PrepColl.sh
+++ parallel_bleeding_edge/bin/StSm_PrepColl.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# Define the location of your tools and ICs
+# Define the location of your tools, ICs and the directory where you
+# have your simulations
 TOOLS=/projects/StarSmasher/starsmasher/parallel_bleeding_edge/tools
 MESA=/projects/StarSmasher/starsmasher/parallel_bleeding_edge/MESA_ICs
+WORK=$HOME/treball/StarSmasher_Collisions
 
-# Initialize the base directory name
+# Initialize the base directory name in the work folder
+cd $WORK
 dir_name="Collision"
 suffix=""
 
@@ -31,4 +34,4 @@ mv $MESA/sph.start* "$dir_name/"
 
 # Step 4: Get into the Collision directory and start the simulation
 cd "$dir_name/"
-echo "Run StSm_run.sh"
+echo "Everything ready. Now call StSm_run.sh"
